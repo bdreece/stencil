33ced1e6b555c469a6f6e85e19f6c4a9cdf21b0b
Brian Reece <bdreece@mtu.edu>
2022-06-17 16:54:01 -0400

    Began implementation of project view components

---
+++ src/lib/ComponentPanel.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { validate_each_argument } from 'svelte/internal';
+
+    let components = [];
+</script>
+
+<nav class="panel">
+    <p class="panel-heading">
+        <span class="icon">
+            <i class="fa-solid fa-cubes" />
+        </span>
+        Components
+    </p>
+    <div class="panel-block">
+        <p class="control has-icons-left">
+            <input class="input" type="text" placeholder="Search" />
+            <span class="icon is-left">
+                <i class="fas fa-search" aria-hidden="true" />
+            </span>
+        </p>
+    </div>
+    {#each components as component}
+        <button class="panel-block is-active">
+            <span class="panel-icon">
+                <i class="fa-solid fa-cube" />
+            </span>
+            {component.value.name}
+        </button>
+    {/each}
+</nav>
---
+++ src/views/CodeView.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let source: string;
+</script>
+
+<pre>
+    <code>
+        {source}
+    </code>
+</pre>
